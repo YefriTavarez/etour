c014d39575e0b7e7586295a530da43f8561e07f9
Yefri Tavarez <yefritavarez@hotmail.com>
2015-11-05 22:51:53 -0400

    Add images and content to the landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,187 @@
-<html>
-	<head>
-		<title>Laravel</title>
-		
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="icon" href="favicon.ico">
+	<title>Dominican Punta Cana Airport Transfers</title>
+	<link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	<link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+<div class="navbar-wrapper">
+	<div class="container">
 
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
-
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
-
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
-
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
-
-			.quote {
-				font-size: 24px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
+		<nav class="navbar navbar-inverse navbar-static-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">
+						eTour
+					</a>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav pull-right">
+						<li><a href="#">Pricing</a></li>
+						<li><a href="#about">Reviews</a></li>
+						<li><a href="#contact">Contact</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Popular Transfers <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Action</a></li>
+								<li><a href="#">Another action</a></li>
+								<li><a href="#">Something else here</a></li>
+								<li role="separator" class="divider"></li>
+								<li class="dropdown-header">Nav header</li>
+								<li><a href="#">Separated link</a></li>
+								<li><a href="#">One more separated link</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</div>
+</div>
+<div id="gallery" class="carousel slide" data-ride="carousel">
+	<ol class="carousel-indicators">
+		<li data-target="#gallery" data-slide-to="0" class="active"></li>
+		<li data-target="#gallery" data-slide-to="1"></li>
+		<li data-target="#gallery" data-slide-to="2"></li>
+	</ol>
+	<div class="carousel-inner" role="listbox">
+		<div class="item active">
+			<img class="first-slide" src="img/sign.jpg" alt="First slide">
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>Don't wait no more</h1>
+                    <p>Let us take care of your transportation while traveling to the Dominican Republic. You'll find your driver at the airport
+                    standing up out of the arrival hall with sign with your name on it.</p>
+					<p><a href="#" class="btn btn-lg btn-primary" role="button">get started!</a></p>
+				</div>
 			</div>
 		</div>
-	</body>
+		<div class="item">
+			<img class="second-slide" src="img/door.jpg" alt="Second slide">
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>Buy your transfer from the comfort of your house and forget about safety while traveling with us.</h1>
+					<p>It is well known that cabs are cheaper than any transfer in the Dominican Republic, but your security is first and more important than everything else.
+						Let eTour take care of your transportation while traveling to the Dominican Republic. </p>
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">book it now!</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="item">
+			<img class="third-slide" src="img/airplane.jpg" alt="Third slide">
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>Travel secure with us</h1>
+					<p>From the moment you book a transfer with us you will feel that you're in good hands with us. Let our most experienced drivers to take care of you and your loved ones.</p>
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">read more</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<a class="left carousel-control" href="#gallery" role="button" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="right carousel-control" href="#gallery" role="button" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
+	</a>
+</div>
+
+<div class="container marketing">
+	<div class="row">
+		<div class="col-lg-4">
+			<img class="img-circle" src="img/punta-cana-airport.jpg" alt="Generic placeholder image">
+			<h2>Punta Cana Airport</h2>
+			<p>Punta Cana is the airport that handle the most people in the DR. Let us take care of your transportation
+				at the best rate of the market. From Punta Cana airport to Bavaro hotels for USD $71 the Round Trip for a group from 1-6 people.
+			</p>
+			<p><a class="btn btn-default" href="#" role="button">Book Now!</a></p>
+		</div>
+		<div class="col-lg-4">
+			<img class="img-circle" src="img/las-americas-airport.jpg" alt="Generic placeholder image">
+			<h2>Las Americas Airport</h2>
+			<p>
+				Travel from Las Americas airport to Bayahibe with the cheapest and most reliable transportation company
+				in the Dominicna Republic for only US $158 Dollars the Round Trip for a group of 1-6 people.
+			</p>
+			<p><a class="btn btn-default" href="#" role="button">Book Now!</a></p>
+		</div>
+		<div class="col-lg-4">
+			<img class="img-circle" src="img/la-romana-airport.jpg" alt="Generic placeholder image">
+			<h2>La Romana Airport</h2>
+			<p>Dominican Tour Express provides affordable and professional private airport transfers with luxurious and comfort
+				able vehicles. We have worked with most of the best companies in the market, the expert in touristic transportation, connecting
+				the main airports in the Dominican Republic by ground with any other destination in the country.</p>
+			<p><a class="btn btn-default" href="#" role="button">Book Now!</a></p>
+		</div>
+	</div>
+
+
+
+	<hr class="featurette-divider">
+
+	<div class="row featurette">
+		<div class="col-md-7">
+			<h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+			<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+		</div>
+		<div class="col-md-5">
+			<img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+		</div>
+	</div>
+
+	<hr class="featurette-divider">
+
+	<div class="row featurette">
+		<div class="col-md-7 col-md-push-5">
+			<h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+			<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+		</div>
+		<div class="col-md-5 col-md-pull-7">
+			<img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+		</div>
+	</div>
+
+	<hr class="featurette-divider">
+
+	<div class="row featurette">
+		<div class="col-md-7">
+			<h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+			<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+		</div>
+		<div class="col-md-5">
+			<img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+		</div>
+	</div>
+
+	<hr class="featurette-divider">
+
+	<footer>
+		<p class="pull-right"><a href="#">Back to top</a></p>
+		<p>&copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+	</footer>
+
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+<script src="https://getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
 </html>
